1afc126b4c90d2223c38c889d809f4939a74b2cf
ChenliangWang <clw.op55@gmail.com>
2022-02-13 15:24:53 -0500

    Create db

--- flaskblg.py
+++ flaskblg.py
@@ -1,9 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    gender = db.Column(db.String(30), nullable=False)
+    full_name = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.id}','{self.fullname}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}','{self.content}')"
+
 
 @app.route("/")
 def hello_world():
